0938ebda228a0e088eabef627c59a2ea0484b969
zhengMa <z1399956473@gmail.com>
2023-01-28 12:18:30 +0800

    wait 1 second before embeddings

--- src/openAiWrapper.ts
+++ src/openAiWrapper.ts
@@ -7,6 +7,8 @@ const completationModel = "text-davinci-003"
 let maxTokens: number =  200
 
 export async function createEmbeddings(content: string): Promise<any> {
+    Promise.resolve(setTimeout(() => { 
+    }, 1000))
     const rs  = await ai?.createEmbedding({"model": embeddingModel, 'input': content, "user": "default"})
     return  rs?.data.data[0].embedding
 }
@@ -22,7 +24,7 @@ export async function createCompletation(content: string): Promise<string> {
 	throw "The response data is undefined"
     }
     
-    return rs.data.choices[0].text
+   return rs.data.choices[0].text
 }
 
 export async function init(openAIKey: string, maxTokensOnCompletation: number): Promise<void> {
